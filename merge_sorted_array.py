0dd67d1f2a428eb16c47fe0b133a7bb3fa6c93d1
Ellie Kuang <49964827+shegocodes@users.noreply.github.com>
2024-09-11 21:30:14 -0700

    Update merge_sorted_array.py

--- merge_sorted_array.py
+++ merge_sorted_array.py
@@ -1,16 +1,25 @@
 """
-Task: You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and 
-two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+Task: You are given two integer arrays nums1 and nums2, sorted in 
+non-decreasing order, and two integers m and n, representing the 
+number of elements in nums1 and nums2 respectively.
 
-Merge nums1 and nums2 into a single array sorted in non-decreasing order. 
+Merge nums1 and nums2 into a single array sorted in non-decreasing 
+order. 
 
-The final sorted array should not be returned by the function, but instead be stored inside 
-the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements
-denote the elements that should be merged, and the last n elements are set to 0 and should 
+The final sorted array should not be returned by the function, but 
+instead be stored inside the array nums1. To accommodate this, nums1 
+has a length of m + n, where the first m elements denote the elements 
+that should be merged, and the last n elements are set to 0 and should 
 be ignored. nums2 has a length of n.
 """
 
 def merge(nums1, m, nums2, n):
+    """
+    Time: O(m + n, linear with respect to the number of elements in both lists).
+    Space: O(1), constant, as no additional space created.
+    """
+    # Use 3 pointers...
+    # p1 points at nums1, p2 points at nums2, p points at merged list
     p1, p2, p = m - 1, n - 1, m + n - 1
     
     # Start merging from the end of nums1
